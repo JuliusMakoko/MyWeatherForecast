4558ebf176f6f9ebf5bcf102c293f5b00f4adc00
julius makoko <juliusmakoko@hotmail.com>
2020-08-05 09:01:43 +0200

    Comments

--- MyWeather/Services/WeatherService.cs
+++ MyWeather/Services/WeatherService.cs
@@ -14,12 +14,10 @@ namespace MyWeather.Services
     public class WeatherService
     {
        
-        //const string WeatherCoordinatesUri = "http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&units={2}&appid=fc9f6c524fc093759cd28d41fda89a1b";
+      
         const string WeatherCoordinatesUri = "http://api.openweathermap.org/data/2.5/weather?lat=&lon={lon}&appid=81f882b652fc019407c3bd1c7ebcd4f5";
-         const string WeatherCityUri = "http://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid=81f882b652fc019407c3bd1c7ebcd4f5";
-        //const string WeatherCityUri = "http://api.openweathermap.org/data/2.5/weather?q=Pretoria&units=metric&cnt=1&APPID=81f882b652fc019407c3bd1c7ebcd4f5";
-         const string ForecaseUri = "http://api.openweathermap.org/data/2.5/forecast?id={0}&units={1}&appid=81f882b652fc019407c3bd1c7ebcd4f5";
-       // const string ForecastUri = "http://api.openweathermap.org/data/2.5/forecast?q=Pretoria&appid=81f882b652fc019407c3bd1c7ebcd4f5";
+        const string WeatherCityUri = "http://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid=81f882b652fc019407c3bd1c7ebcd4f5";
+         const string ForecastUri = "http://api.openweathermap.org/data/2.5/forecast?id={0}&units={1}&appid=81f882b652fc019407c3bd1c7ebcd4f5";
 
         public async Task<WeatherRoot> GetWeather(double latitude, double longitude, Units units = Units.Imperial)
         {
@@ -55,7 +53,7 @@ namespace MyWeather.Services
         {
             using (var client = new HttpClient())
             {
-                var url = string.Format(ForecaseUri, id, units.ToString().ToLower());
+                var url = string.Format(ForecastUri, id, units.ToString().ToLower());
                 var json = await client.GetStringAsync(url);
 
                 if (string.IsNullOrWhiteSpace(json))
